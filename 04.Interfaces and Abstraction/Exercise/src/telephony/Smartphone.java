abe1e316a5b87533014e04f78fac6a1ddb2a7ac3
Martin Lalutov <martinlalutov00@gmail.com>
2025-02-18 23:51:35 +0200

    add telephony solution

---
+++ 04.Interfaces and Abstraction/Exercise/src/telephony/Smartphone.java
@@ -0,0 +1,60 @@
+package telephony;
+
+import java.util.List;
+
+public class Smartphone implements Callable, Browsable {
+    private final List<String> numbers;
+    private final List<String> urls;
+
+    public Smartphone(List<String> numbers, List<String> urls) {
+        this.numbers = numbers;
+        this.urls = urls;
+    }
+
+
+    @Override
+    public String browse() {
+        StringBuilder sb = new StringBuilder();
+        for (String url : this.urls) {
+            if (isUrlValid(url)) {
+                sb.append("Browsing: ").append(url).append("!").append(System.lineSeparator());
+            } else {
+                sb.append("Invalid URL!").append(System.lineSeparator());
+            }
+        }
+
+
+        return sb.toString().trim();
+    }
+
+    @Override
+    public String call() {
+        StringBuilder sb = new StringBuilder();
+        for (String number : this.numbers) {
+            if (isNumberValid(number)) {
+                sb.append("Calling... ").append(number).append(System.lineSeparator());
+            } else {
+                sb.append("Invalid number!").append(System.lineSeparator());
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    private boolean isUrlValid(String url) {
+        for (int i = 0; i < url.length(); i++) {
+            if (Character.isDigit(url.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isNumberValid(String number) {
+        for (int i = 0; i < number.length(); i++) {
+            if (!Character.isDigit(number.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
